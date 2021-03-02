800818ce218f0f9f03324252f0d0c3c46d9dbbf9
Thor William Voster <46726271+WilliamVoster@users.noreply.github.com>
2021-03-02 18:46:05 +0100

    can now fetch countries from API and store locally

--- components/QueryDataForm.js
+++ components/QueryDataForm.js
@@ -1,42 +1,54 @@
 import useLocalStorage from "../hooks/useLocalStorage"
 
 
-
-
 export default function QueryDataForm(){
     
-    const [countries, setCountries] = useLocalStorage("countries", [2])
+    const [countries, setCountries] = useLocalStorage("countries", [])
 
     let queryCountries = async () => {
         const res = await fetch("https://api.covid19api.com/countries")
         const json = await res.json()
-        setCountries([...countries, "test1"])
-        return json;
+
+        setCountries(json)
     }
-    
-    // const countries = queryCountries().json
     
     const action = e => {
         e.preventDefault()
+        console.log("submitted")
+
+        console.log(e)
+
         queryCountries()
-        console.log("tester")
         console.log(countries)
     }
+
+    const clearStorage = () => {
+        setCountries([]);
+        console.log("Cleared storage")
+    }
     
+    const displayStorage = () => {
+        console.log(countries)
+    }
 
 
     return (
-        <form onSubmit={action}>
-            <button>test</button>
-            <input type="text" placeholder="should be dropdown of e.g. countries" />
-            <section>
-                {/* for (let i = 0; i < countries.length; i++) {
-                    const element = countries[i];
-                    <option name="test" value="test"></option>
-                } */}
-            </section>
-            <input type="submit" value="Generate" />
-        </form>
+        <>
+            <button type="button" onClick={clearStorage}>clear local storage</button>
+
+            <button type="button" onClick={displayStorage}>display local storage</button>
+
+            <form onSubmit={action}>
+                <input type="text" placeholder="should be dropdown of e.g. countries" />
+                <section>
+                    {/* for (let i = 0; i < countries.length; i++) {
+                        const element = countries[i];
+                        <option name="test" value="test"></option>
+                    } */}
+                </section>
+                <input type="submit" value="Generate" />
+            </form>
+        </>
     )
 }
 
