d7e63c81972cbfd87b4c1c35caf06d596f23c8ee
Thor William Voster <46726271+WilliamVoster@users.noreply.github.com>
2021-03-02 19:55:15 +0100

    added error message to user if API fails

--- components/QueryDataForm.js
+++ components/QueryDataForm.js
@@ -25,7 +25,12 @@ export default function QueryDataForm(){
 
     let queryCountries = async () => {
         const res = await fetch("https://api.covid19api.com/countries", {mode: "no-cors"})
+        if(!res.ok){
+            console.log("Could not retrieve data from API")
+            return
+        }
         const json = await res.json()
+
 
         const sortedCountries = json.sort((a, b) => compareStrings(a.Slug, b.Slug))
         setCountries(sortedCountries)
@@ -61,11 +66,15 @@ export default function QueryDataForm(){
             <form onSubmit={action}>
                 <select name="selectCountry">
                     <option value="">Select a country</option>
-                    { 
-                         countries.map((c) => {
-                             return <option value={c.Slug}>{c.Country}</option>
-                            // console.log(c.Country)
-                         })
+                    {
+                        countries.map((c) => {
+                            return <option value={c.Slug}>{c.Country}</option>
+                        // console.log(c.Country)
+                        })
+                    }
+                    {
+                        countries.length == 0 &&
+                            <option value="">Could not retrieve countries from API</option>
                     }
                 </select>
 
